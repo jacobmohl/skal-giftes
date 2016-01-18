a29b84e7d210cbc4e1deba1d17373063cc7d0920
Jacob Møhl <hello@jacobmohl.dk>
2016-01-18 21:29:49 +0100

    Add map

--- js/script.js
+++ js/script.js
@@ -56,3 +56,76 @@ jQuery(document).ready(function($){
         );
 	}
 });
+
+
+
+/***************************
+ * 
+ *  Google Maps implementation
+ * 
+ * 
+ * 
+ ****************************/
+ 
+ 
+function createMaker(map, bounds, position) {
+    // Create a marker and set its position.
+    var marker = new google.maps.Marker({
+        map: map,
+        position: position,
+        title: 'Hello World!'
+    });
+    
+    bounds.extend(marker.position);
+    return marker;
+}
+
+var map;
+var myLatLng = [{lat: 55.385389, lng: 10.387459}, {lat: 55.504980, lng: 9.689340}];
+var markers = [];
+  
+  
+function initMap() {
+
+  var bounds = new google.maps.LatLngBounds();
+  
+  // Create a map object and specify the DOM element for display.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: myLatLng[0],
+    scrollwheel: false,
+    draggable: false, 
+    zoomControl: false, 
+    disableDoubleClickZoom: false,
+    zoom: 11,
+    zoomControl: true,
+    mapTypeControl: false,
+    scaleControl: false,
+    streetViewControl: false,
+    rotateControl: false
+  });
+  
+  markers.push(createMaker(map, bounds, myLatLng[0]));
+  markers.push(createMaker(map, bounds, myLatLng[1]));
+  
+  map.fitBounds(bounds);
+
+  /*
+  // Create a marker and set its position.
+  var marker = new google.maps.Marker({
+    map: map,
+    position: myLatLng[0],
+    title: 'Hello World!'
+  });
+  */
+}
+jQuery(document).ready(function($){
+    $("#btn_map_church").click(function(event){
+       map.setCenter(markers[0].getPosition());
+       event.preventDefault();
+    });
+    
+    $("#btn_map_castle").click(function(event){
+       map.setCenter(markers[1].getPosition());
+       event.preventDefault();
+    });    
+})
